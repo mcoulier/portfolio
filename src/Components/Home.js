0ab0a0bcc996dd57ffba2bd302c3acb9f4f5b3ed
Michiel Coulier <michielcoulier@gmail.com>
2020-12-21 20:51:10 +0100

    Text animation main page

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -5,10 +5,14 @@ import {NavLink} from "react-router-dom";
 function Home() {
     return (
         <div className='home'>
-            <h1 className='mainTitle'>Michiel Coulier, Web Developer</h1>
-            <NavLink to="/projects"><button className="projectButton">Projects</button></NavLink>
-        </div>
-    )
+            <h1 className='mainTitle'>
+                <span>M</span><span>i</span><span>c</span><span>h</span><span>i</span><span>e</span><span>l </span>
+                <span>C</span><span>o</span><span>u</span><span>l</span><span>i</span><span>e</span><span>r</span> Junior Web Developer</h1>
+    <NavLink to="/projects">
+        <button className="projectButton">Projects</button>
+    </NavLink>
+</div>
+)
 }
 
 export default Home;
