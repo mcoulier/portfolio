ce14ffcb8554eedad4052535c472d7135847dcc9
Michiel Coulier <michielcoulier@gmail.com>
2020-12-23 13:33:43 +0100

    refactor project component

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -14,19 +14,20 @@ function Header() {
                         <button>Home</button>
                     </NavLink>
 
-                    <a href="https://drive.google.com/file/d/1HDKbmBVWxhTPPQl6qmKo70pVb7cmKa71/view?usp=sharing" download target="_blank" rel="noreferrer">
+                    <a href="https://drive.google.com/file/d/1HDKbmBVWxhTPPQl6qmKo70pVb7cmKa71/view?usp=sharing"
+                       download target="_blank" rel="noreferrer">
                         <button><i className="fa fa-download"/> CV</button>
                     </a>
 
-                        <a href="https://github.com/mcoulier" target="_blank" rel="noreferrer">
-                            <img className="githubIcon" src={githubIcon} alt="githubIcon" width={35} height={35}/>
-                        </a>
-                        <a href="https://www.linkedin.com/in/michiel-coulier/" target="_blank" rel="noreferrer">
-                            <img className="linkedinIcon"  src={linkedinIcon} alt="linkedinIcon" width={35} height={35}/>
-                        </a>
-                        <a href="mailto:michielcoulier@gmail.com">
-                            <img className="mailIcon"  src={mailIcon} alt="mailIcon" width={35} height={35}/>
-                        </a>
+                    <a href="https://github.com/mcoulier" target="_blank" rel="noreferrer">
+                        <img className="githubIcon" src={githubIcon} alt="githubIcon" width={35} height={35}/>
+                    </a>
+                    <a href="https://www.linkedin.com/in/michiel-coulier/" target="_blank" rel="noreferrer">
+                        <img className="linkedinIcon" src={linkedinIcon} alt="linkedinIcon" width={35} height={35}/>
+                    </a>
+                    <a href="mailto:michielcoulier@gmail.com">
+                        <img className="mailIcon" src={mailIcon} alt="mailIcon" width={35} height={35}/>
+                    </a>
 
                 </ul>
             </nav>
--- src/Components/Projects/ProjectsView.js
+++ src/Components/Projects/ProjectsView.js
@@ -1,51 +1,28 @@
 import React from 'react';
 import '../../App.css';
 import Project from "./Project";
+import ProjectData from "./projects.json";
 
-const projectsJSON = {
-    "data": {
-        "Project_1": [
-            {
-                "title": "",
-                "url": "",
-                "image": ""
-            }],
-        "Project_2": [
-            {
-                "title": "",
-                "url": "",
-                "image": ""
-            }],
-        "Project_3": [
-            {
-                "title": "",
-                "url": "",
-                "image": ""
-            }]
-    }
-};
+class ProjectsView extends React.Component {
+    render() {
 
+        const projects = ProjectData.data;
 
-function ProjectsView() {
-    return (
-        <div className='projectsView'>
-            <h1 className="projectsTitle">Projects</h1>
-            <div className='projectsRow'>
-                <div className='projectsColumn'>
-                    <Project title="Ticketing system" url="https://github.com/nikkizol/challenge-workflow"
-                             image="https://portfolio-dac64.web.app/ticket-system.png"/>
-                    <Project title="Pokédex API" url="https://github.com/mcoulier/challenge-pokemon-php"
-                             image="https://portfolio-dac64.web.app/pokedex-api.png"/>
-                </div>
-                <div className='projectsColumn'>
-                    <Project title="MySQL CRUD PHP" url="https://github.com/MonoraxXiV/php-crud"
-                             image="https://portfolio-dac64.web.app/php-crud-mysql.png"/>
-                    <Project title="In Progress" url="https://github.com/mcoulier"
-                             image="https://portfolio-dac64.web.app/project.jpg"/>
+        return (
+            <div className='projectsView'>
+                <h1 className="projectsTitle">Projects</h1>
+                <div className='projectsRow'>
+                    <div className='projectsColumn'>
+                        {
+                            projects.map((value) => {
+                                return <Project title={value.title} url={value.url} image={value.image}/>
+                            })
+                        }
+                    </div>
                 </div>
             </div>
-        </div>
-    )
+        );
+    }
 }
 
 export default ProjectsView;
